1c22de332f148c8a70f9827dcd7032f0f0d3dd18
sunil <bagrahari21@gmail.com>
2022-07-06 00:55:20 +0530

    send page

--- resources/views/admin/warehouse/create.blade.php
+++ resources/views/admin/warehouse/create.blade.php
@@ -2,156 +2,148 @@
 @section('content')
 
 <div class="content-wrapper pb-0">
-
     <div class="card shadow mb-4">
 
-        <x-page-head title="Add Supplier " url="admin/supplier" type="create" />
+        <x-page-head title="Add Warehouse " url="admin/warehouse" type="create" />
 
         <div class="card-body">
             <div class="row">
                 <div class="col-lg-12">
-                    <form class="user" method="POST" action="{{route('warehouse-details-save')}}" enctype="multipart/form-data">
+                    <form id="warehouse" method="POST" action="{{url('admin/warehouse')}}" enctype="multipart/form-data">
                         @csrf
+
+                        <div class="form-row">
+                            <div class="col-md-12">
+                                <h6><span class="mdi mdi-account-check"></span>&nbsp;Persional Details</h6>
+                                <hr>
+                            </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="form-group col-md-4">
+                                <label>Name&nbsp;<span class="text-danger">*</span></label>
+                                <input type="text" class="form-control form-control-sm " name="name" placeholder="Enter Name">
+                                <span id="name_msg" class="text-danger"></span>
+                            </div>
+
+                            <div class="form-group col-md-4">
+                                <label>Email&nbsp;<span class="text-danger">*</span></label>
+                                <input type="email" class="form-control form-control-sm" name="email" placeholder="Enter Email">
+                                <span id="email_msg" class="text-danger"></span>
+                            </div>
+
+                            <div class="form-group col-md-4">
+                                <label>Mobile No&nbsp;<span class="text-danger">*</span></label>
+                                <input type="text" class="form-control form-control-sm" name="mobile" placeholder="Enter Mobile No">
+                                <span id="mobile_msg" class="text-danger"></span>
+                            </div>
+
+                            <div class="form-group col-md-4">
+                                <label>Password&nbsp;<span class="text-danger">*</span></label>
+                                <input type="password" class="form-control form-control-sm" name="password" placeholder="Enter Password">
+                                <span id="password_msg" class="text-danger"></span>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="col-md-12">
+                                <h6><span class="mdi mdi-store "></span>&nbsp;Store Details</h6>
+                                <hr>
+                            </div>
+                        </div>
+
+
                         <div class="form-group row">
-                            <input type="hidden" name="userType" value="warehouse">
-                            <div class="col-sm-6 mb-3">
-                                <label for="">Store Owner</label>
-                                <input type="email" class="form-control" id="email" name="store_owner" placeholder="Store owner">
-                                @error('store_owner')
-                                <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                            </div>
-                            <div class="col-sm-6 mb-3">
+
+                            <div class="form-group col-md-3">
                                 <label for="">Store Name</label>
-                                <input type="text" class="form-control" id="store_name" name="store_name" placeholder="Store name">
-                                @error('store_name')
-                                <span class="text-danger">{{ $message }}</span>
-                                @enderror
+                                <input type="text" class="form-control form-control-sm" name="store_name" placeholder="Store name">
                             </div>
-                            <div class="col-sm-6 mb-3">
+
+                            <div class="form-group col-md-3">
                                 <label for="">Business Email</label>
-                                <input type="email" class="form-control" id="business_email" name="business_email" placeholder="Business email">
-                                @error('business_email')
-                                <span class="text-danger">{{ $message }}</span>
-                                @enderror
+                                <input type="email" class="form-control form-control-sm" name="business_email" placeholder="Business email">
                             </div>
-                            <div class="col-sm-6 mb-3">
+
+                            <div class="form-group col-md-3">
                                 <label for="">GST No</label>
-                                <input type="text" class="form-control" id="gst_no" name="gst_no" placeholder="GST no.">
-                                @error('gst_no')
-                                <span class="text-danger">{{ $message }}</span>
-                                @enderror
+                                <input type="text" class="form-control form-control-sm" name="gst_no" placeholder="GST no.">
                             </div>
-                            <div class="col-sm-6 mb-3">
-                                <label for="">Phone</label>
-                                <input type="text" class="form-control" id="phone" name="phone" placeholder="Phone">
-                                @error('phone')
-                                <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                            </div>
-                            <div class="col-sm-6 mb-3">
+
+                            <div class="form-group col-md-3">
                                 <label for="">Mobile</label>
-                                <input type="text" class="form-control" id="mobile" name="mobile" placeholder="Mobile">
-                                @error('mobile')
-                                <span class="text-danger">{{ $message }}</span>
-                                @enderror
+                                <input type="text" class="form-control form-control-sm" name="store_mobile" placeholder="Mobile">
                             </div>
-                            <div class="col-sm-6 mb-3">
+
+                            <div class="form-group col-md-3">
                                 <label for="">Country</label>
-                                <select class="form-select form-control" name="country">
+                                <select class="form-select form-control form-control-sm" name="country">
                                     <option value="">-Select Here-</option>
                                     <option value="India">India</option>
                                     <option value="USA">USA</option>
                                 </select>
-                                @error('country')
-                                <span class="text-danger">{{ $message }}</span>
-                                @enderror
                             </div>
-                            <div class="col-sm-6 mb-3">
+
+                            <div class="form-group col-md-3">
                                 <label for="">State</label>
-                                <select class="form-select form-control" name="state">
+                                <select class="form-select form-control form-control-sm" name="state">
                                     <option value="">-Select Here-</option>
                                     <option value="Delhi">Delhi</option>
                                     <option value="Goa">Goa</option>
                                     <option value="UP">UP</option>
                                 </select>
-                                @error('state')
-                                <span class="text-danger">{{ $message }}</span>
-                                @enderror
                             </div>
-                            <div class="col-sm-6 mb-3">
+
+                            <div class="form-group col-md-3">
                                 <label for="">City</label>
-                                <select class="form-select form-control" name="city">
+                                <select class="form-select form-control form-control-sm" name="city">
                                     <option value="">-Select Here-</option>
                                     <option value="Delhi">Delhi</option>
                                     <option value="Noida">Noida</option>
                                     <option value="Agra">Agra</option>
                                 </select>
-                                @error('city')
-                                <span class="text-danger">{{ $message }}</span>
-                                @enderror
                             </div>
-                            <div class="col-sm-6 mb-3">
+
+                            <div class="form-group col-md-3">
                                 <label for="">Pincode</label>
-                                <input type="text" class="form-control" id="pincode" name="pincode" placeholder="pincode">
-                                @error('pincode')
-                                <span class="text-danger">{{ $message }}</span>
-                                @enderror
+                                <input type="text" class="form-control form-control-sm" name="pincode" placeholder="pincode">
                             </div>
-                            <div class="col-sm-6 mb-3">
+
+                            <div class="form-group col-md-6">
                                 <label for="">Store Address</label>
-                                <textarea class="form-control" id="store_address" name="store_address" rows="4" cols="50" placeholder="Store Address"></textarea>
-                                @error('store_address')
-                                <span class="text-danger">{{ $message }}</span>
-                                @enderror
+                                <textarea class="form-control form-control-sm" name="store_address" rows="4" cols="50" placeholder="Store Address"></textarea>
                             </div>
-                            <div class="col-sm-6 mb-3">
-                                <label for="">Store Description</label>
-                                <textarea class="form-control" id="store_description" name="store_description" rows="4" cols="50" placeholder="Store Address"></textarea>
-                                @error('store_description')
-                                <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                            </div>
-                            <div class="col-sm-6 mb-3">
-                                <label for="">Logo</label>
-                                <input type="file" name="logo" class="form-control" accept="image/*,.pdf">
-                                @error('logo')
-                                <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                            </div>
-                            <div class="col-sm-6 mb-3">
-                                <label for="">Store Cover Photo</label>
-                                <input type="file" name="store_cover_photo" class="form-control" accept="image/*,.pdf">
-                                @error('store_cover_photo')
-                                <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                            </div>
-                            <div class="col-sm-3 mb-3">
+
+                            <div class="form-group col-md-3">
                                 <label for="">Status</label>
-                                <select class="form-select form-control" name="status">
+                                <select class="form-select form-control form-control-sm" name="status">
                                     <option value="1">Active</option>
                                     <option value="0">Deactive</option>
                                 </select>
-                                @error('status')
-                                <span class="text-danger">{{ $message }}</span>
-                                @enderror
                             </div>
-                            <div class="col-sm-3 mb-3">
+
+                            <div class="form-group col-md-3">
                                 <label for="">Verified Store</label>
-                                <select class="form-select form-control" name="verified_store">
+                                <select class="form-select form-control form-control-sm" name="verified_store">
                                     <option value="1">Active</option>
                                     <option value="0">Deactive</option>
                                 </select>
-                                @error('status')
-                                <span class="text-danger">{{ $message }}</span>
-                                @enderror
                             </div>
+
+                            <div class="form-group col-md-3">
+                                <label for="">Logo</label>
+                                <input type="file" name="flies[logo]" class="form-control form-control-sm" accept="image/*,.pdf">
+                            </div>
+
+                            <div class="form-group col-md-3">
+                                <label for="">Store Cover Photo</label>
+                                <input type="file" name="file[store_cover_photo]" class="form-control form-control-sm" accept="image/*,.pdf">
+                            </div>
+
                         </div>
                         <div class="form-group">
-                            <div class="col-sm-2" style="float: right;">
-                                <button type="submit" class="btn btn-primary btn-user btn-block">
-                                    {{ __('Submit') }}
-                                </button>
+                            <div class="col-sm-12 text-center">
+                              <input type="submit" value="Submit" class="btn btn-success">
+                              <button type="reset" class="btn btn-warning"><span class="mdi mdi-rotate-left"></span>&nbsp;Reset</button>
                             </div>
                         </div>
                     </form>
@@ -161,4 +153,56 @@
     </div>
 
 </div>
+@push('script')
+<script>
+    $('form#warehouse').submit(function(e) {
+        e.preventDefault();
+        formData = new FormData(this);
+        let url = $(this).attr('action');
+        $('.cover-loader').removeClass('d-none');
+        $('.h-body').addClass('d-none');
+        axios.post(url, formData)
+            .then(function(response) {
+                res = response.data;
+
+                $('.cover-loader').addClass('d-none');
+                $('.h-body').removeClass('d-none');
+
+                /*Start Validation Error Message*/
+                $('span.custom-text-danger').html('');
+                $.each(res.validation, (index, msg) => {
+                    $(`#${index}_msg`).html(`${msg}`);
+                })
+                /*Start Validation Error Message*/
+
+                /*Start Status message*/
+                if (res.status == 'success' || res.status == 'error') {
+                    Swal.fire(
+                        `${res.status}!`,
+                        res.msg,
+                        `${res.status}`,
+                    )
+                }
+                /*End Status message*/
+
+                //for reset all field
+                if (res.status == 'success') {
+                    $('form#warehouse')[0].reset();
+                    setTimeout(() => {
+                        // location.reload();
+                    }, 1000);
+                }
+
+            })
+            .catch(function(error) {
+                console.log(error);
+                Swal.fire(
+                    `Error!`,
+                    error,
+                    `error`,
+                );
+            });
+    })
+</script>
+@endpush
 @endsection
